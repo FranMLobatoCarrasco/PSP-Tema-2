1924e3b17d52eb677bfd2842030c7788a2b50156
Francisco Manuel Lobato Carrasco <franciscom.lobato@iesnervion.es>
2023-01-22 21:07:28 +0100

    commit diario

--- ejerciciolibro/EstudianteMetodosSynchronized.java
+++ ejerciciolibro/EstudianteMetodosSynchronized.java
@@ -4,64 +4,59 @@ import java.util.Random;
 
 public class EstudianteMetodosSynchronized implements Runnable {
 
-	// Los libros son el recurso compartido
-	public static boolean[] libros = new boolean[9];
-	public static Object o = new Object();
+    // Los libros son el recurso compartido
+    public static boolean[] libros = new boolean[9];
+    public static Object o = new Object();
 
-	public synchronized void reservaLibros(int libro1, int libro2) {
-		while (libros[libro1] == true || libros[libro2] == true) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+    public synchronized void reservaLibros(int libro1, int libro2) {
+        while (libros[libro1] == true || libros[libro2] == true) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // Reservo los libros
+        libros[libro1] = true;
+        libros[libro2] = true;
+    }
 
-		// Reservo los libros
-		libros[libro1] = true;
-		libros[libro2] = true;
-	}
+    public synchronized void liberaLibros(int libro1, int libro2) {
+        libros[libro1] = false;
+        libros[libro2] = false;
+        this.notifyAll();
+    }
 
-	public synchronized void liberaLibros(int libro1, int libro2) {
-		libros[libro1] = false;
-		libros[libro2] = false;
-		this.notifyAll();
-	}
-	
-	@Override
-	public void run() {
-		try {
-			while (true) {
-				int libro1 = new Random().nextInt(9);
-				int libro2 = new Random().nextInt(9);
-				while (libro2 == libro1) {
-					libro2 = new Random().nextInt(9);
-				}
-				reservaLibros(libro1, libro2);
-				
-				System.out.println(
-						Thread.currentThread().getName() + " tiene reservados los libros " + libro1 + " y " + libro2);
-				Thread.sleep((long) (Math.random() * 3000));
-				System.out.println(Thread.currentThread().getName() + " ha terminado de leer.");
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                int libro1 = new Random().nextInt(9);
+                int libro2 = new Random().nextInt(9);
+                while (libro2 == libro1) {
+                    libro2 = new Random().nextInt(9);
+                }
+                reservaLibros(libro1, libro2);
+                System.out.println(
+                        Thread.currentThread().getName() + " tiene reservados los libros " + libro1 + " y " + libro2);
+                Thread.sleep((long) (Math.random() * 3000));
+                System.out.println(Thread.currentThread().getName() + " ha terminado de leer.");
+                liberaLibros(libro1, libro2);
+                Thread.sleep(1000);
+            }
+        } catch (
 
-				liberaLibros(libro1, libro2);
+                InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-				Thread.sleep(1000);
-			}
-		} catch (
-
-		InterruptedException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static void main(String[] args) {
-		EstudianteMetodosSynchronized e = new EstudianteMetodosSynchronized();
-		for (int i = 1; i <= 4; i++) {
-			Thread hilo = new Thread(e);
-			hilo.setName("Estudiante " + i);
-			hilo.start();
-		}
-	}
+    public static void main(String[] args) {
+        EstudianteMetodosSynchronized e = new EstudianteMetodosSynchronized();
+        for (int i = 1; i <= 4; i++) {
+            Thread hilo = new Thread(e);
+            hilo.setName("Estudiante " + i);
+            hilo.start();
+        }
+    }
 }
--- ejerciciolibro/Libro.java
+++ ejerciciolibro/Libro.java
@@ -1,25 +1,24 @@
 package ejerciciolibro;
 
 public class Libro {
-	public static boolean[] libros = new boolean[9];
+    public static boolean[] libros = new boolean[9];
 
-	public synchronized void reservaLibros( int libro1, int libro2) {
-		while (libros[libro1] == true || libros[libro2] == true) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		// Reservo los libros
-		libros[libro1] = true;
-		libros[libro2] = true;
-	}
+    public synchronized void reservaLibros(int libro1, int libro2) {
+        while (libros[libro1] == true || libros[libro2] == true) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // Reservo los libros
+        libros[libro1] = true;
+        libros[libro2] = true;
+    }
 
-	public synchronized void liberaLibros(int libro1, int libro2) {
-		libros[libro1] = false;
-		libros[libro2] = false;
-		this.notifyAll();
-	}
-
+    public synchronized void liberaLibros(int libro1, int libro2) {
+        libros[libro1] = false;
+        libros[libro2] = false;
+        this.notifyAll();
+    }
 }
